72b4be92fa418959c8e615455fb9372143fe78a3
Joseph <josephmt1176@gmail.com>
2024-09-02 11:35:39 -0400

    initial commit

---
+++ src/lib/utils/constants.ts
@@ -0,0 +1,8 @@
+export const onTapBounceAnimation = {
+    scale: 0.95,
+    transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 10,
+    },
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,14 +12,14 @@ const config = {
     ],
     prefix: "",
     theme: {
-        container: {
-            center: true,
-            padding: "2rem",
-            screens: {
-                "2xl": "1400px",
-            },
-        },
         extend: {
+            container: {
+                center: true,
+                padding: "2rem",
+            },
+            screens: {
+                xs: "425px",
+            },
             colors: {
                 border: "hsl(var(--border))",
                 input: "hsl(var(--input))",

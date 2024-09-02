64a696ddc84615a2dbbc714c2ed67b53f0f68849
Joseph <josephmt1176@gmail.com>
2024-09-02 14:30:28 -0400

    made InView more dynamic

--- src/components/animations/InView.tsx
+++ src/components/animations/InView.tsx
@@ -7,18 +7,25 @@ import { useRef } from "react";
 export default function InView({
     children,
     className = "",
+    direction = "left",
 }: {
     children: React.ReactNode;
     className?: string;
+    direction?: "left" | "right";
 }) {
     const ref = useRef(null);
     const isInView = useInView(ref, { once: false });
+
+    const directions = {
+        left: "translateX(-100%)",
+        right: "translateX(100%)",
+    };
 
     return (
         <div
             ref={ref}
             style={{
-                transform: isInView ? "none" : "translateX(-200px)",
+                transform: isInView ? "none" : directions[direction],
                 opacity: isInView ? 1 : 0,
                 transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
             }}
